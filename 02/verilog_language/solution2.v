2bfbab8aa63e8e65dd658a08fc1b273b59492d9f
pritha-nandy <prithanandy.6002@gmail.com>
2026-08-24 19:47:09 +0530

    Update solution2.v

--- 02/verilog_language/solution2.v
+++ 02/verilog_language/solution2.v
@@ -76,4 +76,55 @@ module top_module(
 endmodule
 
 //module fadd
+module top_module (
+    input [31:0] a,
+    input [31:0] b,
+    output [31:0] sum
+);//
+    wire cout;
+    add16 a1(a[15:0],b[15:0],1'b0,sum[15:0],cout);
+    add16 a2(a[31:16],b[31:16],cout,sum[31:16],);
+    
 
+endmodule
+
+module add1 ( input a, input b, input cin,   output sum, output cout );
+    assign sum=a^b^cin;
+    assign cout=a&b|b&cin|a&cin;
+ 
+endmodule
+
+//module cseladd
+module top_module(
+    input [31:0] a,
+    input [31:0] b,
+    output [31:0] sum
+);
+    wire cout;
+    wire [31:16]sum0,sum1;
+    add16 a1(a[15:0],b[15:0],1'b0,sum[15:0],cout);
+    add16 a2(a[31:16],b[31:16],1'b0,sum0[31:16],);
+    add16 a3(a[31:16],b[31:16],1'b1,sum1[31:16],);
+    always @(*)
+        begin
+            case(cout)
+                1'b0:sum[31:16]=sum0[31:16];
+                1'b1:sum[31:16]=sum1[31:16];
+            endcase
+        end
+   
+
+endmodule
+
+//module addsub
+module top_module(
+    input [31:0] a,
+    input [31:0] b,
+    input sub,
+    output [31:0] sum
+);
+    wire [31:0]z;
+    wire cout;
+    assign z=b^{32{sub}};
+    add16 a1(a[15:0],z[15:0],sub,sum[15:0],cout);
+    add16 a2(a[31:16],z[31:16],cout,sum[31:16],);
